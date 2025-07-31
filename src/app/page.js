86fff2f905fda7a873428c8308d712cd4d4faa49
JSanJuan21 <jorgesanjuanfrancisco@gmail.com>
2025-07-31 10:34:42 +0100

    general changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,94 @@
 import Image from "next/image";
+import CardImage from "@/components/CardImage";
+import CardInfo from "@/components/CardInfo";
+import QandA from "@/components/QandA";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-white text-black">
+      <main className="grid gap-10 row-start-2 items-center text-center ">
+        <div>
+          {/* Logo */}
+          <Image
+            className="dark:invert mx-auto"
+            src="/XYZ.svg"
+            alt="XYZ logo"
+            width={300}
+            height={300}
+            priority
+          />
+        </div>
+        {/* Títulos principales */}
+        <div>
+          <h1 className="text-4xl sm:text-5xl font-bold leading-tight drop-shadow-md">
+            Impulsamos tu negocio,
+          </h1>
+          <h1 className="text-4xl sm:text-5xl font-bold leading-tight drop-shadow-md">
+            sin importar el tamaño de tu empresa
+          </h1>
+        </div>
+        <div>
+          {/* Descripción */}
+          <p className="text-base sm:text-lg leading-relaxed text-gray-700 px-4">
+            XYZ Digital nace con la misión de ayudar a empresas a crecer en el mundo digital.<br />
+            En una era donde la visibilidad online lo es todo, trabajamos para que negocios —desde emprendedores locales hasta grandes corporaciones—<br />
+            logren destacar en los motores de búsqueda y conectar con sus clientes ideales.
+          </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          {/* Botón */}
+          <button className="mt-4 bg-black text-white px-6 py-3 rounded-full text-sm sm:text-base hover:bg-gray-800 transition">
+            Nuestros servicios <span className="ml-2">★</span>
+          </button>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-x-4">
+          <CardImage src={"/designer.jpg"} />
+          <CardImage src={"/designer2.jpg"}/>
+          <CardImage src={"/plan.jpg"} />
+          <CardImage src={"/meeting.jpg"} />
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-x-4">
+          <CardImage src={"/super.jpg"} />
+          <CardImage src={"/worker.jpg"}/>
+          <CardImage src={"/worker2.jpg"} />
+          <CardImage src={"/worker3.jpg"} />
+        </div>
+        <div>
+          <h1 className="text-4xl sm:text-5xl font-bold leading-tight drop-shadow-md">
+            A través del Kit Digital, ofrecemos servicios
+          </h1>
+          <h1 className="text-4xl sm:text-5xl font-bold leading-tight drop-shadow-md">
+            de alta calidad sin barreras económicas.
+          </h1>
+        </div>
+        <div className="bg-[url(/test1.svg)] bg-auto">
+          <div className="grid grid-cols-3 gap-y-4 ml-15 mr-15">
+            <CardInfo />
+            <CardInfo />
+            <CardInfo />
+            <CardInfo />
+            <CardInfo />
+            <CardInfo />
+          </div>
+          <div className="mt-5">
+            <h1 className="text-4xl sm:text-5xl font-bold leading-tight drop-shadow-md">
+              Nos involucramos con cada cliente
+            </h1>
+            <h1 className="text-4xl sm:text-5xl font-bold leading-tight drop-shadow-md">
+              como si fuera parte de nuestro equipo.
+            </h1>
+          </div>
+          <div className="grid grid-cols-3 mt-5 ml-15 mr-15">
+            <CardInfo />
+            <CardInfo />
+            <CardInfo />
+          </div>
+        </div>
+        <div className="bg-black p-6 min-h-screen">
+          <h1 className="text-gray-50 text-9xl ">Preguntas y Respuestas</h1>
+          <QandA />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+
